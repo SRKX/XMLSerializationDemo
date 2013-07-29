c0a232551e23733478317bdad81088c88e058821
Jeremie Smaga <jeremie@smaga.ch>
2013-07-29 19:16:48 +0200

    Initial project commit

---
+++ XMLSerializationDemo/Instruments/Instrument.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLSerializationDemo.Instruments
+{
+    public abstract class Instrument
+    {
+        public abstract string Name { get; }
+    }
+}
---
+++ XMLSerializationDemo/Instruments/Stock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLSerializationDemo.Instruments
+{
+    /// <summary>
+    /// Represents a Stock
+    /// </summary>
+    public class Stock:Instrument
+    {
+        private string _name;
+
+        public override string Name
+        {
+            get { return _name; }
+        }
+    }
+}
